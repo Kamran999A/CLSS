7d3ac52b39fc00ea8e3a4c063c495c3f9db60c12
Kamran999A <85412897+Kamran999A@users.noreply.github.com>
2021-06-20 18:20:28 +0400

    asd

---
+++ ConsoleApp6/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Partial_Classes
+{
+    public class Point 
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Point () { }
+        public Point (double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Point (X) = {X}");
+            Console.WriteLine($"Point (Y) = {Y}");
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Partial_Classes
+{
+    class Program
+    {
+        static void Run()
+        {
+            /*=============================================================*/
+            /* ······ TASK 1 ······ */
+            /*=============================================================*/
+            //var point  = new Point (596, 782);
+            //point .Show();
+
+            /*=============================================================*/
+            /* ······ TASK 2 ······ */
+            /*=============================================================*/
+
+            //try                       // minimumun maksimumdan boyuk olub olmadiqini yoxlayir
+            //{
+            //    var counter = new Counter(200, 100);
+            //}
+            //catch (Exception content)
+            //{
+            //    Console.WriteLine(content);
+            //    Console.WriteLine();
+            //}
+
+            //var counterSecond = new Counter(0, 100);
+
+            //for (double i = 0; i < 150; i++)
+            //{       
+            //    if ( i == 100)
+            //    {
+            //        Console.WriteLine();
+            //    }
+            //    Console.Write(counterSecond.Increment() + " ");
+            //}
+            //Console.WriteLine();
+
+            /*=============================================================*/
+            /* ······ TASK 3 ······ */
+            /*=============================================================*/
+
+            //var first = new Fraction(2.5, 1);
+            //var second = new Fraction(0.5, 1);
+            //first.Show();
+            //second.Show();
+            //Console.WriteLine();
+            //Console.WriteLine(first / second);
+            //Console.WriteLine(first * second);
+            //Console.WriteLine(first + second);
+            //Console.WriteLine(first - second);
+            //Console.WriteLine();
+
+            /*=============================================================*/
+            /* ······ TASK 4 ······ */
+            /*=============================================================*/
+
+            //var car = new Car(1, "Jeep", "AMERICAN ENGINE COMPANY", "WRANGLER", new DateTime(2020, 01, 01));
+            //car.Show();
+        }
+        static void Main(string[] args)
+        {
+            Run();
+        }
+    }
+}
